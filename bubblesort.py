b4e7bdada534e09ddc441b829ed3351d426c84a2
elijah2340 <elijahjohn2340@gmail.com>
2022-06-30 08:27:06 +0100

    added requirements.txt

--- bubblesort.py
+++ bubblesort.py
@@ -2,10 +2,10 @@ mylist = [7,1,8,3,4,2]
 temporary = 0
 for i in range(len(mylist)-1):
     for j in range(len(mylist)-1):
-        if mylist[i] > mylist[j+1]:
-            temporary = mylist[i]
-            mylist[i] = mylist[j+1]
-            mylist[j+1] = temporary
+        if mylist[i] < mylist[j]:
+            temporary = mylist[j]
+            mylist[j] = mylist[i]
+            mylist[i] = temporary
 
 
 print(mylist)
